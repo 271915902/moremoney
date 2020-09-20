a15e95fba272dc57c4f109e3c30c6a3b6007d9ac
yuxin <271915902@qq.com>
2020-09-20 23:05:04 +0800

    使用全局tagList

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -6,3 +6,26 @@ declare module "*.svg"{
   const content:string;
   export default content;
 }
+
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number ;
+  createdAt?: Date ;
+}
+type Tag = {
+  id: string;
+  name: string;
+}
+type TagListModel = {
+  data: Tag[];
+  create: (name: string) => 'success' | 'duplicated' ;
+  update: (id: string, name: string) => 'success' | 'not found' | 'duplicated';
+  remove: (id: string) => boolean;
+  save: () => void;
+}
+
+interface Window {
+  tagList: Tag[];
+} 
